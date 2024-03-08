948dd7bcbc43aabe2ffd1a3838853be531933a47
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2024-03-08 23:41:59 +0700

    update

--- Array&Hashing/49. Group Anagrams/main.go
+++ Array&Hashing/49. Group Anagrams/main.go
@@ -2,17 +2,49 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main(){
 	fmt.Println(groupAnagrams([]string{"eat","tea","tan","ate","nat","bat"}))
+	fmt.Println("eat" == "tea")
+	
+	x := []rune("eat")
+	y := []rune("tea")
+
+	sort.Slice(x, func(i, j int) bool {
+		return x[i] <= x[j]
+	})
+
+	sort.Slice(y, func(q, c int) bool {
+		return y[q] <= y[c]
+	})
+
+	fmt.Println(string(x) == string(y))
 }
 
 func groupAnagrams(strs []string) [][]string {
-	for _, word := range strs {
-		fmt.Println(word)
-	}
-	return [][]string{{"tansan","sanan"},{"hahaha","eieie","yoyoyo"}}
+
+	// seen := make(map[int]int)
+
+	// for _ , word := range strs {
+	// 	wSlice := []rune(word)
+
+	// 	sort.Slice(wSlice, func(i, j int)  bool {
+	// 		return wSlice[i] <= wSlice[j]
+	// 	})
+
+	// 	if 
+
+	// }
+
+	// sort.Slice( strs, func(i, j int) bool {
+	// 	return strs[i] <= strs[j]
+	// })
+
+	fmt.Println(strs)
+
+	return [][]string {{"hello","yoyoy"},{"yoyo"}}
 
 }
 
