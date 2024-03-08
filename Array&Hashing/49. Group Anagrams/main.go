cc209329e94c2f2e0d7b1e40afe5e4f9dc3560ac
tankubopa777 <84582195+tankubopa777@users.noreply.github.com>
2024-03-09 04:53:52 +0700

    Update main.go

--- Array&Hashing/49. Group Anagrams/main.go
+++ Array&Hashing/49. Group Anagrams/main.go
@@ -24,27 +24,26 @@ func main(){
 }
 
 func groupAnagrams(strs []string) [][]string {
+    anagramMap := make(map[string][]string) // Map to store anagrams
 
-	// seen := make(map[int]int)
+    for _, word := range strs {
+        sortedWord := string(sortRunes(word)) // Sort runes and convert to string
+        anagramMap[sortedWord] = append(anagramMap[sortedWord], word)  
+    }
 
-	// for _ , word := range strs {
-	// 	wSlice := []rune(word)
+    result := [][]string{} // Slice to store the groups of anagrams
+    for _, group := range anagramMap {
+        result = append(result, group)
+    }
 
-	// 	sort.Slice(wSlice, func(i, j int)  bool {
-	// 		return wSlice[i] <= wSlice[j]
-	// 	})
-
-	// 	if 
-
-	// }
-
-	// sort.Slice( strs, func(i, j int) bool {
-	// 	return strs[i] <= strs[j]
-	// })
-
-	fmt.Println(strs)
-
-	return [][]string {{"hello","yoyoy"},{"yoyo"}}
-
+    return result
 }
 
+// Helper function to sort the runes of a word
+func sortRunes(str string) []rune {
+    runes := []rune(str)
+    sort.Slice(runes, func(i, j int) bool {
+        return runes[i] < runes[j]
+    })
+    return runes
+}
